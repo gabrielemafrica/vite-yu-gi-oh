e9385b9d39a0198deb8c38c2b256e2a9bd40ab2b
gabrielemafrica <gabriele.ga@live.it>
2023-06-05 17:58:04 +0300

    dipendenze

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppHeader',
+    props: ['message'],
+}
+
+</script>
+
+<template>
+    <header class="container">
+        <h1 class="my-5">{{ message }}</h1>
+    </header>
+</template>
+
+<style scoped lang="scss">
+h1 {
+    font-weight: 700;
+}
+</style>
