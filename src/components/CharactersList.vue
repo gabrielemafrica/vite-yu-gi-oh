bfd0e78801b3d952cd1f2d9a4707493a266c4643
gabrielemafrica <gabriele.ga@live.it>
2023-06-05 18:17:56 +0300

    ultimo

---
+++ src/components/CharactersList.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+    name: CharactersList,
+}
+</script>
+<template>
+    <section class="container">
+        <!-- card -->
+        <div class="row">
+            <div class="col-2">
+                <img class="img-fluid mb-3" src="" alt="">
+                <h4>nome</h4>
+                <div>tipo</div>
+
+            </div>
+        </div>
+    </section>
+</template>
+<style scoped lang="scss">
+@use '../styles/partials/variables' as *;
+
+h4 {
+    text-transform: uppercase;
+}
+</style>
